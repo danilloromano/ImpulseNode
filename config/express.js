97dfc536612b3ee81a00ffffd97e08ccf335f186
Danillo Romano <danilloferomano@gmail.com>
2016-09-15 16:04:07 -0700

    criando o projeto impulseNode

---
+++ config/express.js
@@ -0,0 +1,18 @@
+//var express = require('express');
+//var load = require('express-load');
+//var bodyParser = require('body-parser');
+//var expressValidator = require('express-validator');
+
+
+//	module.exports = function(){
+//		var app = express();
+//		app.set('view engine','ejs');
+//		app.set('views','./app/views');
+//		app.use(bodyParser.urlencoded({extended: true}));
+//		app.use(expressValidator());
+//carrega automaticamente tudo dentro de routs e infra.
+//		load('routs',{cwd: 'app'})
+//		.then('infra')
+//		.into(app);
+//	return app;
+//	};

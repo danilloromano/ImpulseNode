dd39b5536b2e89f142c4d120b1d5b97c8e3c9b84
Danillo Romano <danilloferomano@gmail.com>
2016-09-21 09:44:15 -0700

    criando a factory,express load e a rota devolvendo os dados

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
-var express = require('express');
 
-var app = express();
+var app = require('./config/express')();
 
-app.set('view engine','ejs');
-app.get('/cliente',function(req,res){
-		res.render('cliente/cadastro_cliente');
-});
 
-app.listen(3000,function(){
-	console.log("Rodando");
+app.listen(3000, function(){
+    console.log("Servidor rodando");
 });
--- config/express.js
+++ config/express.js
@@ -1,13 +1,29 @@
+var express = require('express');
+var load = require('express-load');
+
+module.exports = function() {
+	
+	var app = express();
+	app.set('view engine', 'ejs');
+	app.set('views','./app/views');
+	
+	load('routes',{cwd: 'app'})
+	.then('infra')
+	.into(app);
+
+    return app;
+}
+
 //var express = require('express');
 //var load = require('express-load');
 //var bodyParser = require('body-parser');
 //var expressValidator = require('express-validator');
 
 
-//	module.exports = function(){
-//		var app = express();
-//		app.set('view engine','ejs');
-//		app.set('views','./app/views');
+	//module.exports = function(){
+	//var app = express();
+	//app.set('view engine','ejs');
+	//app.set('views','./app/views');
 //		app.use(bodyParser.urlencoded({extended: true}));
 //		app.use(expressValidator());
 //carrega automaticamente tudo dentro de routs e infra.
